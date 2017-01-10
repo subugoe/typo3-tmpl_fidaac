6f2848ab66d90c83e40f8dbc70cc239d612ee8e5
tschaef <tschaef1@gwdg.de>
2017-01-10 17:19:47 +0100

    Add ESLint with config for frontend tests, lint JS
    Using Airbnb's config with 4 instead of 2 spaces.

--- tests/test-oldLive/test-oldLive-general.js
+++ tests/test-oldLive/test-oldLive-general.js
@@ -1,28 +1,27 @@
 // This test is to test the general availability of the website
-casper.options.viewportSize = {width: 1024, height: 768};
+casper.options.viewportSize = { width: 1024, height: 768 };
 
 casper.test.begin('Test for original AAC Web presence', 1, function suite(test) {
-
-  casper.start('https://aac.sub.uni-goettingen.de', function () {
-
-    casper.echo('Opened page with title \"' + this.getTitle() + '"');
-    casper.capture('./tests/images/aac-oldLive-home.png');
-
-    test.assertVisible('.pz2-searchField');
-  });
-  casper.then(function () {
-    this.fillSelectors('.pz2-searchForm', {
-      'input[id="pz2-field-all"]': 'holmes'
+    casper.start('https://aac.sub.uni-goettingen.de', function () {
+        casper.echo('Opened page with title "' + this.getTitle() + '"');
+        casper.capture('./tests/images/aac-oldLive-home.png');
+        test.assertVisible('.pz2-searchField');
     });
-  });
-  casper.then(function () {
-      this.waitForSelector('.pz2-recordCount', function () {
-      casper.echo('Number of results: ' + this.getHTML('.pz2-recordCount', false));
-    });
-  });
 
-  casper.run(function () {
-    test.done();
-  });
+    casper.then(function () {
+        this.fillSelectors('.pz2-searchForm', {
+            'input[id="pz2-field-all"]': 'holmes',
+        });
+    });
+
+    casper.then(function () {
+        this.waitForSelector('.pz2-recordCount', function () {
+            casper.echo('Number of results: ' + this.getHTML('.pz2-recordCount', false));
+        });
+    });
+
+    casper.run(function () {
+        test.done();
+    });
 });
 
