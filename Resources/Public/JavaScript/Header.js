dec64229f716437d4619cdbc9153ad61f4376dfa
snaegle <naegle@sub.uni-goettingen.de>
2017-07-02 19:58:34 +0200

    Implement alternative Menu with arrows an JavaScript in L Related to: AAC-504

--- Resources/Public/JavaScript/Header.js
+++ Resources/Public/JavaScript/Header.js
@@ -1,28 +1,11 @@
-$(document).ready(function(){
-
-    // create a botton as replacement for the menu
-    var createMenuButton = function () {
-        mbt = '<div class="menuButton">' +
-            '<svg><use xlink:href="/typo3conf/ext/tmpl_fidaac/Resources/Public/Images/symbol-defs.svg#icon-bars"/></svg>' +
-            'Menu' +
-            '</div>';
-
-        $('.navigation').append('<div class="menuButton">' +
-            '<svg><use xlink:href="/typo3conf/ext/tmpl_fidaac/Resources/Public/Images/symbol-defs.svg#icon-bars"/></svg>' +
-            'Menu' +
-            '</div>');
-
-        $('.moreButton').on('click', function () {
-            console.log('clicked')
-        });
-    };
+$(document).ready(function () {
 
     /**
      * change sizes of elements for different media sizes
      * Workaround until grid-system compatible with most browsers
      * Text labels hide when screen smaller than 768 and reappear at hover
      */
-    changeHeader = function() {
+    changeHeader = function () {
         if (window.innerWidth > 960) {
             ['home','search','language'].forEach(function(el) {
                 $('.header_' + el + '-text').removeClass('removed');
@@ -59,9 +42,12 @@ $(document).ready(function(){
                 $('.header_' + el).addClass('removed');
             });
             $('.navigation').addClass('removed');
-            //changeHeader();
             return;
         }
     }
+
+    $(window).resize(function() {
+        changeHeader();
+    });
 
 });
