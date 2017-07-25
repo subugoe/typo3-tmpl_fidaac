68c5d2556c8f2dc85a3bd851aa681ae110ec28c5
Nils Windisch <nils@windisch.me>
2017-07-25 08:48:24 +0200

    Fix

--- build.js
+++ build.js
@@ -4,8 +4,7 @@ buildify()
     .concat([
         'Resources/Private/JavaScript/Header.Js',
         'Resources/Private/JavaScript/Navigation.Js',
-        'Resources/Private/JavaScript/Main.Js',
-        'node_modules/jquery/dist/jquery.min.js'
+        'Resources/Private/JavaScript/Main.Js'
     ])
     .save('Resources/Public/JavaScript/concatenated.js');
 
