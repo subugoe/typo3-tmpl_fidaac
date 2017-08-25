d507b8e6e5e07125074fb9ac56d0d7474003c86b
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2017-08-25 12:13:19 +0200

    Adjust configurations for TYPO3 8
    Related: AAC-68

--- Configuration/System/realurl_conf.php
+++ Configuration/System/realurl_conf.php
@@ -8,35 +8,35 @@ $path = __DIR__ . '/../../../../realurl_NewsPageIds.php';
 include_once $path;
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT'] = [
-	'init' => [
-		'appendMissingSlash' => 'ifNotFile,redirect[301]',
-		'respectSimulateStaticURLs' => 0,
-		'enableCHashCache' => true,
-		'enableUrlDecodeCache' => false,
-		'enableUrlEncodeCache' => false,
+    'init' => [
+        'appendMissingSlash' => 'ifNotFile,redirect[301]',
+        'respectSimulateStaticURLs' => 0,
+        'enableCHashCache' => true,
+        'enableUrlDecodeCache' => false,
+        'enableUrlEncodeCache' => false,
         'reapplyAbsRefPrefix' => true,
-		'adminJumpToBackend' => true,
-		'emptyUrlReturnValue' => '/',
-	],
-	'preVars' => [
-		0 => [
-			'GETvar' => 'L',
-			'valueMap' => [
-				'de' => '1',
-			],
-			'noMatch' => 'bypass',
-		],
-	],
-	'pagePath' => [
-		'type' => 'user',
-		'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
-		'spaceCharacter' => '-',
-		'languageGetVar' => 'L',
-		'expireDays' => '3',
-		'rootpage_id' => $rootPageUid,
-		'firstHitPathCache' => 1
-	],
-	'fixedPostVars' => [
+        'adminJumpToBackend' => true,
+        'emptyUrlReturnValue' => '/',
+    ],
+    'preVars' => [
+        0 => [
+            'GETvar' => 'L',
+            'valueMap' => [
+                'de' => '1',
+            ],
+            'noMatch' => 'bypass',
+        ],
+    ],
+    'pagePath' => [
+        'type' => 'user',
+        'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
+        'spaceCharacter' => '-',
+        'languageGetVar' => 'L',
+        'expireDays' => '3',
+        'rootpage_id' => $rootPageUid,
+        'firstHitPathCache' => 1
+    ],
+    'fixedPostVars' => [
         'newsDetailConfiguration' => [
             [
                 'GETvar' => 'tx_news_pi1[action]',
@@ -105,13 +105,20 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT'] = [
                 ]
             ]
         ],
-        $newsDetailsIdHome[0] => $newsDetailsIdHome[1], // Einzelansicht Aktuelles
-        $newsDetailsIdArchive[0] => $newsDetailsIdArchive[1], // Einzelansicht Archiv
-        $newsCategoryId[0] => $newsCategoryId[1], // Liste nach Kategorie
-        $newsDetailsIdCollectionAmerican[0] => $newsDetailsIdCollectionAmerican[1], // Einzelansicht American Studies
-        $newsDetailsIdCollectionAustralian[0] => $newsDetailsIdCollectionAustralian[1], // Einzelansicht Australian and New Zealand Studies
-        $newsDetailsIdCollectionCanadian[0] => $newsDetailsIdCollectionCanadian[1], // Einzelansicht Canadian Studies
-        $newsDetailsIdCollectionEnglish[0] => $newsDetailsIdCollectionEnglish[1], // Einzelansicht Englisch; Britisch and Irish Studies
+        $newsDetailsIdHome[0] => $newsDetailsIdHome[1],
+        // Einzelansicht Aktuelles
+        $newsDetailsIdArchive[0] => $newsDetailsIdArchive[1],
+        // Einzelansicht Archiv
+        $newsCategoryId[0] => $newsCategoryId[1],
+        // Liste nach Kategorie
+        $newsDetailsIdCollectionAmerican[0] => $newsDetailsIdCollectionAmerican[1],
+        // Einzelansicht American Studies
+        $newsDetailsIdCollectionAustralian[0] => $newsDetailsIdCollectionAustralian[1],
+        // Einzelansicht Australian and New Zealand Studies
+        $newsDetailsIdCollectionCanadian[0] => $newsDetailsIdCollectionCanadian[1],
+        // Einzelansicht Canadian Studies
+        $newsDetailsIdCollectionEnglish[0] => $newsDetailsIdCollectionEnglish[1],
+        // Einzelansicht Englisch; Britisch and Irish Studies
     ],
     'fileName' => [
         'defaultToHTMLsuffixOnPrev' => 0,
