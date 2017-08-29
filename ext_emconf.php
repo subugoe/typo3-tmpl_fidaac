bc49da5c4fde783f13a5203c0b8e1c1186fd707e
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2017-08-29 13:29:00 +0200

    Unify TypoScript file suffix to t3s

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,18 +2,18 @@
 /************************************************************************
  * Extension Manager/Repository config file for ext "tmpl_ssgaac".
  ************************************************************************/
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'typo3 tmpl fidaac',
     'description' => 'Template and Configuration for FID AAC',
     'category' => 'extension',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '7.6.0-8.99.99',
             'fluid_styled_content' => '7.6.0-8.99.99'
-        ),
-        'conflicts' => array(
-        ),
-    ),
+        ],
+        'conflicts' => [
+        ],
+    ],
     'state' => 'alpha',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -22,4 +22,4 @@ $EM_CONF[$_EXTKEY] = array(
     'author_email' => 'blache@sub.uni-goettingen.de',
     'author_company' => 'Goettingen State and University Library Germany',
     'version' => '0.0.1',
-);
+];
