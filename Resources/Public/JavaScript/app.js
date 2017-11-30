1b6eeb4c7c58484a6bdc4d3faca17217bdb99345
snaegle <naegle@sub.uni-goettingen.de>
2017-11-30 18:33:06 +0100

    Fix menu hide error

--- Resources/Public/JavaScript/app.js
+++ Resources/Public/JavaScript/app.js
@@ -4,32 +4,18 @@ $(document).ready(function () {
 
     var searchForm = $('#search-form');
 
-    // we use realurl, so we have different paths, depending on language
-    var lang = $('html').attr('lang');
-    var getAction = function getAction(search) {
-        if (lang === 'de') {
-            if (search === 'metasearch') {
-                return '/de/suche/kataloge';
-            }
-            return '/de/suche/website';
-        }
-        if (search === 'metasearch') {
-            return '/search/metasearch-engine';
-        }
-        return '/search/website';
-    };
     var searchEngineSettingsMetasearch = {
-        'action': getAction('metasearch'),
+        'action': '/search/metasearch-engine',
         'id': 'choice-pazpar2',
         'name': 'tx_pazpar2_pazpar2[queryString]',
-        'query': getAction('metasearch') + '?tx_pazpar2_pazpar2[useJS]=yes&tx_pazpar2_pazpar2[queryString]='
+        'query': '/search/metasearch-engine?tx_pazpar2_pazpar2[useJS]=yes&tx_pazpar2_pazpar2[queryString]='
     };
 
     var searchEngineSettingsWebsite = {
-        'action': getAction('website'),
+        'action': '/search/website/',
         'id': 'choice-website',
         'name': 'q',
-        'query': getAction('website') + '/?q='
+        'query': '/search/website/?q='
     };
 
     var initializeSearchForm = function initializeSearchForm() {
@@ -199,6 +185,22 @@ $(document).ready(function () {
     });
 
     /**
+     * Remove whole menu when mouse is outside
+     */
+    $('.navigation_default').hoverIntent({
+        over: function over() {
+            // nothing to do
+        },
+        out: function out() {
+            $('.navigation_default-submenuContainer-outer').hide();
+            $('.navigation_default-submenuItems').removeClass('hover');
+            $('.clone').remove();
+            $('.cloned').removeClass('cloned');
+        },
+        timeout: 250
+    });
+
+    /**
      * Stuff related to scrolling and page up button
      */
     $(window).on('scroll', function (event) {
@@ -228,6 +230,7 @@ $(document).ready(function () {
      * The body is not displayed by default, only after javascript is done it is finally displayed
      * This removes jumping elements
      */
+    $('.navigation_default-submenuContainer-outer').hide();
     // hide, so that space is preserved
     $('.toTop_inner').css('visibility', 'hidden');
 });
